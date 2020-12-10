7768f60f82ee934a9b942582299b708a7a667b9e
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-09 20:45:39 -0500

    I've changed the side bar and added a card underneath the jumbotron to show the information of the current city.

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ var queryURL ="";
 $.ajax({
 
 });
+
+var initialBtn = $(".initialBtn");
+
+// initalBtn.on("click", function () {
+//   let hour = $(this).siblings(".hour").text();
+//   let input = $(this).siblings("#input").val();
+//   // console.log(input);
+//   // console.log(hour);
+
+//   localStorage.setItem(hour, input);
+// });
+
+// function Storing (){
+//     $(".hour").each(function(){
+//         let time = $(this).text();
+//         let text = localStorage.getItem(time);
+//     if (text !== null) {
+//         $(this).siblings("textarea").val(text);
+//     }
+//     });
+// }
+// Storing();
