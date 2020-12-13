039a39ce45c01da1450949c037eabd2f26691e49
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-13 14:14:04 -0500

    I was able to add my API key and test out whether it was working but hard coding a city and getting it's temp.

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-var APIkey= "";
-var queryURL ="";
+var APIkey= "ca7397c46053898d7c12dc596fd7cf71";
+//eventually this will be what the user types in
+var cityName= "Rome"
+var queryURL =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
 $.ajax({
+    method: "GET",
+    url: queryURL
 
+}).then(function(response) {
+    console.log(response)
+    var temp = response.main.temp
+    console.log(temp)
 });
 
 var initialBtn = $(".initialBtn");
+
+//One API 3 queries
+//
+
+
+
+
 
 // initalBtn.on("click", function () {
 //   let hour = $(this).siblings(".hour").text();
