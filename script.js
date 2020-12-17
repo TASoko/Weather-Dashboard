385762108715e6c3f916c42deb4def93059f4fcf
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-16 22:40:08 -0500

    I've succuessfully added the temp, wind speed and humidity to the 5 day forecast cards.I have icons showing up however they may be wrong.

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ $(document).ready(function () {
         })
     );
 
-
-    var APIkey = "ca7397c46053898d7c12dc596fd7cf71";
-    //eventually this will be what the user types in
-    //var cityName = "Rome";
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=Rome&appid=${APIkey}`;
-
     var initialBtn = $(".initialBtn");
+    initialBtn.on("click", function (event) {
+        event.preventDefault();
 
-    //initialBtn.click(function () {
+        var cityName = $("#cityValue").val();
+        
+        var APIkey = "ca7397c46053898d7c12dc596fd7cf71";
+        //eventually this will be what the user types in
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
 
 
 
@@ -32,18 +32,28 @@ $(document).ready(function () {
             console.log(tempF)
 
 
+            $("#Psearched").append("<li>" + response.name + "</li>");
+
             $(".city").html("<h1>" + response.name + " Weather Details</h1>");
             $(".wind").text("Wind Speed: " + response.wind.speed + " MPH ");
             $(".humidity").text("Humidity: " + response.main.humidity + " % ");
             $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
 
+
             console.log("Wind Speed: " + response.wind.speed);
             console.log("Humidity: " + response.main.humidity);
+
+            // var icon = response.weather[0].icon;
+            // var iconURL = "http://openweathermap.org.img/wn" + icon + ".png";
+
+
             //console.log("Temperature (F): " + tempF);
 
-            var lat = (response.coord.lat);
-            var lon = (response.coord.lon);
+
+
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
 
             var queryURL1 = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIkey}`;
 
@@ -57,9 +67,24 @@ $(document).ready(function () {
             })
 
 
+            var queryURL3 = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,daily,alerts&appid=${APIkey}`
+            $.ajax ({
+                method: "GET",
+                url: queryURL3
+            }).then (function (response){
+                console.log(response);
+                for (i = 0; i < 5; i++) {
+                    var icon = response.daily[i].weather[0].icon;
+                    var iconURL = "https://openweathermap.org/img/w/" + icon + ".png";
+   
+                    $("#icon"+i).attr('src', iconURL);
+                }
+            })
+
+
         });
-        
-        var queryURL2 = `http://api.openweathermap.org/data/2.5/forecast?q=Rome&appid=${APIkey}`;
+
+        var queryURL2 = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIkey}`;
 
         $.ajax({
             method: "GET",
@@ -68,81 +93,39 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response)
             console.log(response.list)
-    
-        let forecast = [];
-        for (var i=0; i<=response.list.length;i += 8){
-           forecast.push(i);
-        }
+
+            let forecast = [];
+            for (var i = 0; i <= response.list.length; i += 8) {
+                forecast.push(i);
+            }
+
         
-        console.log(forecast)
-        for (var i=0; i<forecast.length; i++) {
-             $("#card1")
-         }
-         var tempF = (response.list[forecast[0]].main.temp - 273.15) * 1.80 + 32;
+            console.log(forecast)
 
-         $(".wind1").text("Wind Speed: " + response.list[forecast[0]].wind.speed + " MPH ");
-         $(".humidity1").text("Humidity: " + response.list[forecast[0]].main.humidity + " % ");
-         $(".tempF1").text("Temperature (F) " + tempF.toFixed(2));
-     
-
-         $(".wind2").text("Wind Speed: " + response.list[forecast[1]].wind.speed + " MPH ");
-         $(".humidity2").text("Humidity: " + response.list[forecast[1]].main.humidity + " % ");
-
-       
-       
-         $(".wind3").text("Wind Speed: " + response.list[forecast[2]].wind.speed + " MPH ");
-         $(".humidity3").text("Humidity: " + response.list[forecast[2]].main.humidity + " % ");
+            for (var i = 0; i < forecast.length; i++) {
+                console.log(i)
+                //forecast.forEach(function ())
+                var tempF = (response.list[forecast[i]].main.temp - 273.15) * 1.80 + 32;
 
 
 
-         $(".wind4").text("Wind Speed: " + response.list[forecast[3]].wind.speed + " MPH ");
-         $(".humidity4").text("Humidity: " + response.list[forecast[3]].main.humidity + " % ");
 
+                $(".wind" + i).text("Wind Speed: " + response.list[forecast[i]].wind.speed + " MPH ");
+                $(".humidity" + i).text("Humidity: " + response.list[forecast[i]].main.humidity + " % ");
+                $(".tempF" + i).text("Temperature (F) " + tempF.toFixed(2));
 
+                console.log(tempF)
 
-         $(".wind5").text("Wind Speed: " + response.list[forecast[4]].wind.speed + " MPH ");
-         $(".humidity5").text("Humidity: " + response.list[forecast[4]].main.humidity + " % ");
+                 
 
+               
+            
 
-            //for loop to grab for time of the date called - filtering
-            //0, 7, 15,
+            }
 
-            //dot push into a new array
-            //create blank array - forecast
-            //each card id 1-5
-            //new for loop, run through new array to run through length, i+ when targeting id
-            //target 5 
         })
 
-    //});
-});   
-// for (var i=0; i<response.length;i+8){
-//     $(".card").text(response[i].Day)
-//         $("<div>").text(response[i].Time)
-// }
+        //});
+    })
 
-//One API 3 queries
-//
-
-
-
-
-
-// initalBtn.on("click", function () {
-//   let hour = $(this).siblings(".hour").text();
-//   let input = $(this).siblings("#input").val();
-//   // console.log(input);
-//   // console.log(hour);
-
-//   localStorage.setItem(hour, input);
-// });
-
-// function Storing (){
-//     $(".hour").each(function(){
-//         let time = $(this).text();
-//         let text = localStorage.getItem(time);
-//     if (text !== null) {
-//         $(this).siblings("textarea").val(text);
-//     }
-//     });
-// 
+})
