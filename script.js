80e21d45ec8e77c4f03b33ea48a5578b47768121
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-19 00:01:09 -0500

    I was able to show the current date's icon and also get the dates on the 5 day forecast cards. What's left is the buttons on the aside.

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
-
-function naturalstate (){
+  function naturalstate() {
     var lastSearched = localStorage.getItem("lastSearched");
-    if (lastSearched !== null){
-        $("#cityValue").val(lastSearched);
-        render();
+    if (lastSearched !== null) {
+      $("#cityValue").val(lastSearched);
+      render();
     }
-}
-naturalstate ();
+  }
+  naturalstate();
   $("#currentDay").text(
     luxon.DateTime.local().toLocaleString({
       weekday: "long",
@@ -18,7 +17,6 @@ naturalstate ();
 
   function render() {
     var cityName = $("#cityValue").val();
-
 
     var APIkey = "ca7397c46053898d7c12dc596fd7cf71";
     //eventually this will be what the user types in
@@ -32,38 +30,34 @@ naturalstate ();
       var tempF = (response.main.temp - 273.15) * 1.8 + 32;
       console.log(tempF);
 
-    //   var inputBtn = $("<button>");
-      var link = $("<nav-link>");
-      //link.attr("href", "#");
-      link.text(response.name);
-      link.addClass("linkName");
+    //   //   var inputBtn = $("<button>");
+    //   var link = $("<nav-link>");
+    //   //link.attr("href", "#");
+    //   link.text(response.name);
+    //   link.addClass("linkName");
 
-    //   $("#Psearched").append(inputBtn);
-      $("#v-pills-home-tab").append(link);
+    //   //   $("#Psearched").append(inputBtn);
+    //   $("#v-pills-home-tab").append(link);
 
-    //   inputBtn.text(response.name);
-    //   inputBtn.addClass("searchBar");
-    //   $("#Psearched").empty();
-      
-
+      //   inputBtn.text(response.name);
+      //   inputBtn.addClass("searchBar");
+      //   $("#Psearched").empty();
 
       $(".city").html("<h1>" + response.name + " Weather Details</h1>");
       $(".wind").text("Wind Speed: " + response.wind.speed + " MPH ");
       $(".humidity").text("Humidity: " + response.main.humidity + " % ");
       $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-
-
-    //   let city = [];
-    //   for (i=0;)
-
       console.log("Wind Speed: " + response.wind.speed);
       console.log("Humidity: " + response.main.humidity);
 
+      var icon1 = response.weather[0].icon;
+      var iconURL1 = "https://openweathermap.org/img/w/" + icon1 + ".png";
+
+      $("#bigIcon").attr("src", iconURL1);
+
       // var icon = response.weather[0].icon;
       // var iconURL = "http://openweathermap.org.img/wn" + icon + ".png";
-
-      //console.log("Temperature (F): " + tempF);
 
       var lat = response.coord.lat;
       var lon = response.coord.lon;
@@ -76,11 +70,42 @@ naturalstate ();
       }).then(function (response) {
         $(".UV").text("UV Index: " + response.value);
         console.log(response);
-      });
 
-      if (response.value < 2){
-          
-      }
+        UVindexEl = $(".UV");
+        var UVindex = response.value;
+        if (UVindex <= 2) {
+          UVindexEl.removeClass("moderate");
+          UVindexEl.removeClass("high");
+          UVindexEl.removeClass("very-high");
+          UVindexEl.removeClass("extreme");
+          UVindexEl.addClass("low");
+        } else if (UVindex >= 2 && UVindex <= 5) {
+          UVindexEl.removeClass("low");
+          UVindexEl.removeClass("high");
+          UVindexEl.removeClass("very-high");
+          UVindexEl.removeClass("extreme");
+          UVindexEl.addClass("moderate");
+        } else if (UVindex >= 5 && UVindex <= 7) {
+          UVindexEl.removeClass("low");
+          UVindexEl.removeClass("moderate");
+          UVindexEl.removeClass("very-high");
+          UVindexEl.removeClass("extreme");
+          UVindexEl.addClass("high");
+        } else if (UVindex >= 7 && UVindex <= 10) {
+          UVindexEl.removeClass("low");
+          UVindexEl.removeClass("moderate");
+          UVindexEl.removeClass("high");
+          UVindexEl.addClass("extreme");
+          UVindexEl.addClass("very-high");
+        } else {
+          UVindexEl.removeClass("low");
+          UVindexEl.removeClass("moderate");
+          UVindexEl.removeClass("high");
+          UVindexEl.removeClass("very-high");
+          UVindexEl.addClass("extreme");
+        }
+        });
+    });
 
       // var queryURL3 = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,daily,alerts&appid=${APIkey}`
       // $.ajax ({
@@ -92,7 +117,7 @@ naturalstate ();
 
       //     }
       // })
-    });
+    
 
     var queryURL2 = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIkey}`;
 
@@ -113,6 +138,14 @@ naturalstate ();
       for (var i = 0; i < forecast.length; i++) {
         console.log(i);
         //forecast.forEach(function ())
+        var date = new Date(response.list[forecast[i]].dt*1000).toLocaleDateString('en-US',{
+          month:'2-digit',
+          day: '2-digit',
+          year: 'numeric',
+        })
+        $(".card-body" + i).append(`
+        <h5 class="card-title">${date}</h5>`)
+
         var tempF = (response.list[forecast[i]].main.temp - 273.15) * 1.8 + 32;
 
         $(".wind" + i).text(
@@ -129,32 +162,34 @@ naturalstate ();
         var iconURL = "https://openweathermap.org/img/w/" + icon + ".png";
 
         $("#icon" + i).attr("src", iconURL);
+
+      
       }
     });
-    }
- 
+   
 
-    var initialBtn = $(".initialBtn");
-    initialBtn.on("click", function (event) {
-    localStorage.setItem("lastSearched", $("#cityValue").val())
-      event.preventDefault();
-      render();
-      //});
-    });
+//   };
 
-    // var searchBtn = $(".searchBar");
-    var linkN= $(".linkName")
+  var initialBtn = $(".initialBtn");
+  initialBtn.on("click", function (event) {
+    localStorage.setItem("lastSearched", $("#cityValue").val());
+    event.preventDefault();
+    render();
+    //});
+  });
 
-    linkN.click(function (e){
-        alert("Button is good");
-        e.preventDefault();
+  // var searchBtn = $(".searchBar");
+  var linkN = $(".linkName");
 
-        // var city = $("#cityValue").val().trim();
-        // console.log("#cityValue");
-        // render ();
-    });
-  
-  
+  linkN.click(function (e) {
+    alert("Button is good");
+    e.preventDefault();
+
+    // var city = $("#cityValue").val().trim();
+    // console.log("#cityValue");
+    // render ();
+  });
+}
 });
 //var prevS = $("#Psearched").val();
 //if cityName === prevS {
